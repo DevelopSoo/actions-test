72d9b1592e30da0a98d4e2d0f964275ef258b66a
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-04-18 14:24:14 +0900

    threshold 수정

--- jest.config.ts
+++ jest.config.ts
@@ -15,20 +15,6 @@ const config: Config = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testPathIgnorePatterns: ["<rootDir>/tests/", "<rootDir>/tests-examples/"],
-  coverageThreshold: {
-    global: {
-      statements: 80,
-      branches: 70,
-      functions: 80,
-      lines: 80,
-    },
-    "src/utils/user.ts": {
-      statements: 90,
-      branches: 85,
-      functions: 95,
-      lines: 95,
-    },
-  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
