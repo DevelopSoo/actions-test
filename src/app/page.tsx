c180a593e10db70012b394b0b876ed5460869fec
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-05-04 01:29:40 +0900

    lodash 설치 후 cache 확인

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
   const [values, setValues] = useState({ title: '', content: '' });
 
   useEffect(() => {
-    console.log('useEffect');
     const fetchPosts = async () => {
       const res = await fetch('/api/posts');
       if (!res.ok) {
